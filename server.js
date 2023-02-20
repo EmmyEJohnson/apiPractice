09a3b1832dbab9c3459b23132a63e94e74f57243
Emily Johnson <emelizajo22@gmail.com>
2023-02-19 18:03:07 -0800

    add middleware to server.js file

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
+//** External Modules
+// require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+// const logger = require('morgan');
+// const session = require('express-session');
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const methodOverride = require('method-override');
 
+
+//** Internal Modules
+
+//** Connect to DB
+
+//** Instanced Module
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+//** Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// instead of using morgan to log...i.e. app.use(logger('morgan'));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+// app.use(express.static('public'));
+// app.use(session({
+  //   secret: '',
+  //   resave: false,
+  //   saveUninitialized: true
+  // }));
+  // app.use(passport.initialize());
+  // app.use(passport.session());
+  // app.use(methodOverride('_method'));
+  
+  //** App Configuration
+  // app.set('view engine', 'ejs');
+  
+  //** Routes
+  
+  
+  // pass a "preflight" check or JSON request on chrome
 app.use(cors());
 
-// pass a "preflight" check or JSON request on chrome
 app.options("*", (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
@@ -14,4 +51,8 @@ app.options("*", (req, res, next) => {
   res.send(200);
 }); 
 
+//** System Variable
+const PORT = process.env.PORT || 3000;
+
+//** Server Bind
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
